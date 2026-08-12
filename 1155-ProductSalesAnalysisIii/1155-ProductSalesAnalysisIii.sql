797a343da6da64bd0011e7c45d13a5c5e52cf3ad
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:08:25 +0530

    Create 1155-ProductSalesAnalysisIii

---
+++ 1155-ProductSalesAnalysisIii/1155-ProductSalesAnalysisIii.sql
@@ -0,0 +1,14 @@
+-- Last updated: 12/08/2026, 19:08:24
+SELECT
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM Sales s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) t
+ON s.product_id = t.product_id
+AND s.year = t.first_year;
