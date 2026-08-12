a1c1502f4cd6626ebf3f8fb6a2eec7fb44a8846b
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:15:56 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.java
@@ -0,0 +1,16 @@
+// Last updated: 12/08/2026, 19:15:55
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+}
