5ad7f5a3bbd8b2aaa0ce2dbed9bb02c465a0cf7e
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 12:12:55 +0530

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/12/2026, 12:12:54 PM
+SELECT
+    u.name,
+    IFNULL(SUM(r.distance),0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r
+ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name;
