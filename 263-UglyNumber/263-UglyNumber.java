ee48a12be6b46c6ddaabd300627787bc9c4e2c53
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:13:24 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 12/08/2026, 19:13:23
+class Solution {
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+
+        int[] factors = {2, 3, 5};
+
+        for (int factor : factors) {
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+
+        return n == 1;
+    }
+}
