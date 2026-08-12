c673f7a9972a10ed7de79a5257164c8baa5ebae5
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:13:15 +0530

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.java
@@ -0,0 +1,22 @@
+// Last updated: 12/08/2026, 19:13:15
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
