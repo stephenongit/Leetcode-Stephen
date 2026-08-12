06b5712d98932e3166ef9a1bcac56b9116b1a84a
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:10:59 +0530

    Create 610-TriangleJudgement

---
+++ 610-TriangleJudgement/610-TriangleJudgement.sql
@@ -0,0 +1,13 @@
+-- Last updated: 12/08/2026, 19:10:59
+SELECT
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z
+         AND x + z > y
+         AND y + z > x
+        THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
