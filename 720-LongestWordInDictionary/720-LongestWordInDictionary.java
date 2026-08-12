e21e0dc161c263e7d4ae216ce8f3fc68bf5ac242
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:10:22 +0530

    Create 720-LongestWordInDictionary

---
+++ 720-LongestWordInDictionary/720-LongestWordInDictionary.java
@@ -0,0 +1,22 @@
+// Last updated: 12/08/2026, 19:10:22
+import java.util.*;
+
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+
+        Set<String> set = new HashSet<>();
+        String ans = "";
+
+        for (String word : words) {
+            if (word.length() == 1 || set.contains(word.substring(0, word.length() - 1))) {
+                set.add(word);
+                if (word.length() > ans.length()) {
+                    ans = word;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
