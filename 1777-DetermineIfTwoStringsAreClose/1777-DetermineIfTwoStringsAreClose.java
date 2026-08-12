06c4ebf15e3b2b9e0cd7e60ab054999b2ede1080
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 12:12:38 +0530

    Create 1777-DetermineIfTwoStringsAreClose

---
+++ 1777-DetermineIfTwoStringsAreClose/1777-DetermineIfTwoStringsAreClose.java
@@ -0,0 +1,32 @@
+// Last updated: 8/12/2026, 12:12:37 PM
+import java.util.*;
+
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0) != (freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        return Arrays.equals(freq1, freq2);
+    }
+}
