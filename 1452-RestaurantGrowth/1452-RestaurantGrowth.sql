422b92d6b937196d335cfa7229be12cd301c0a52
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:07:20 +0530

    Create 1452-RestaurantGrowth

---
+++ 1452-RestaurantGrowth/1452-RestaurantGrowth.sql
@@ -0,0 +1,20 @@
+-- Last updated: 12/08/2026, 19:07:17
+SELECT
+    visited_on,
+    SUM(amount) OVER (
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    ROUND(
+        AVG(amount) OVER (
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ),
+        2
+    ) AS average_amount
+FROM (
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+) t
+LIMIT 1000000 OFFSET 6;
