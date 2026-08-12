b5aa10b8165739b5f8e005729ebfa7c86bba186c
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:12:08 +0530

    Create 461-HammingDistance

---
+++ 461-HammingDistance/461-HammingDistance.java
@@ -0,0 +1,15 @@
+// Last updated: 12/08/2026, 19:12:07
+class Solution {
+    public int hammingDistance(int x, int y) {
+
+        int xor = x ^ y;
+        int count = 0;
+
+        while (xor != 0) {
+            count += xor & 1;
+            xor >>= 1;
+        }
+
+        return count;
+    }
+}
