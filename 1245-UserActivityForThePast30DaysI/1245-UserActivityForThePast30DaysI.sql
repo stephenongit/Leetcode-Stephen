293375088aa307656671f68fd5c8e2c6922ad5d3
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:08:01 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,6 @@
+-- Last updated: 12/08/2026, 19:08:01
+SELECT activity_date AS day,
+       COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;
