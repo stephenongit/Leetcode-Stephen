f11603a860153aac22600e6d0523c8463cff2a2d
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:12:45 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.java
@@ -0,0 +1,22 @@
+// Last updated: 12/08/2026, 19:12:45
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long left = 1;
+        long right = num;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
