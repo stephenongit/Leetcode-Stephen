376d6a3fcd95008ec96d0bfb72412c5e5fd7f02b
stephenongit <188839243+stephenongit@users.noreply.github.com>
2026-08-12 19:10:06 +0530

    Create 745-FindSmallestLetterGreaterThanTarget

---
+++ 745-FindSmallestLetterGreaterThanTarget/745-FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,19 @@
+// Last updated: 12/08/2026, 19:10:06
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return letters[left % letters.length];
+    }
+}
